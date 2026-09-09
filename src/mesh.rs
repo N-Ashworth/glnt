ad1712f4186e4ecd91b285890b9094463c13edbf
n-ash158 <ashworth8320@sps81.org>
2026-09-08 21:43:54 -0700

    Added builder pattern for shader

--- src/mesh.rs
+++ src/mesh.rs
@@ -11,6 +11,7 @@ pub struct Mesh {
 
     index_count: i32,
 }
+
 impl Mesh {
     pub fn new(
         vertices: Vec<f32>,
--- src/shader.rs
+++ src/shader.rs
@@ -2,219 +2,297 @@ use std::ffi::{CString};
 use std::fs;
 use std::ptr;
 
+pub struct ShaderBuilder {
+	vertex: Option<String>,
+	geom: Option<String>,
+	frag: Option<String>,
+}
+
+impl ShaderBuilder {
+	fn new() -> Self {
+		Self {
+			None,
+			None,
+			None,
+		}
+	}
+
+	fn vertex_from_file(&self, path: &str) -> Self {
+		let vertex_source = fs::read_to_string(path)
+			.unwrap_or_else(|e| {
+				panic!(
+					"Failed to read vertex shader '{}': {}",
+					path, e
+				)
+			});
+
+		Self {
+			vertex: Some(vertex_source),
+			geom: self.geom.clone(),
+			frag: self.frag.clone(),
+		}
+	}
+
+	fn geometry_from_file(&self, path: &str) -> Self {
+		let geom_source = fs::read_to_string(path)
+			.unwrap_or_else(|e| {
+				panic!(
+					"Failed to read geometry file '{}': {}",
+					path, e
+				)
+			});
+
+		Self {
+			vertex: self.vertex.clone(),
+			geom: Some(geom_source),
+			frag: self.frag.clone(),
+		}
+	}
+
+	fn fragment_from_file(&self, path: &str) -> Self {
+		let frag_source = fs::read_to_string(path)
+			.unwrap_or_else(|e| {
+				panic!(
+					"Failed to read fragment file '{}': {}",
+					path, e
+				)
+			});
+
+		Self {
+			vertex: self.vertex.clone(),
+			geom: self.geom.clone(),
+			frag: Some(frag_source),
+		}
+	}
+
+	fn compile_shader(
+		source: &str,
+		shader_type: u32,
+	) -> u32 {
+		let c_source = CString::new(source)
+			.expect("Shader source contained a null byte");
+
+		let shader;
+
+		unsafe {
+			shader = gl::CreateShader(shader_type);
+
+			gl::ShaderSource(
+				shader,
+				1,
+				&c_source.as_ptr(),
+				ptr::null(),
+			);
+
+			gl::CompileShader(shader);
+
+			let mut success = 0;
+
+			gl::GetShaderiv(
+				shader,
+				gl::COMPILE_STATUS,
+				&mut success,
+			);
+
+			if success == 0 {
+				let mut len = 0;
+
+				gl::GetShaderiv(
+					shader,
+					gl::INFO_LOG_LENGTH,
+					&mut len,
+				);
+
+				let mut buffer = vec![0u8; len as usize];
+
+				gl::GetShaderInfoLog(
+					shader,
+					len,
+					ptr::null_mut(),
+					buffer.as_mut_ptr() as *mut i8,
+				);
+
+				let error = String::from_utf8_lossy(&buffer);
+
+				panic!("Shader compilation failed:\n{}", error);
+			}
+		}
+
+		shader
+	}
+
+	fn complain_about_shader() {
+		let mut len = 0;
+
+		gl::GetProgramiv(
+			program,
+			gl::INFO_LOG_LENGTH,
+			&mut len,
+		);
+
+		let mut buffer = vec![0u8; len as usize];
+
+		gl::GetProgramInfoLog(
+			program,
+			len,
+			ptr::null_mut(),
+			buffer.as_mut_ptr() as *mut i8,
+		);
+
+		let error = String::from_utf8_lossy(&buffer);
+
+		panic!("Shader program linking failed:\n{}", error);
+	}
+
+	fn create_program(v_shader: Option<u32>, g_shader: Option<u32>, f_shader: Option<u32>) -> u32 { unsafe {
+		let program = gl::CreateProgram();
+
+		if let Some(shader) = v_shader {
+			gl::AttachShader(program, shader);
+		}
+
+		if let Some(shader) = g_shader {
+			gl::AttachShader(program, shader);
+		}
+
+		if let Some(shader) = f_shader {
+			gl::AttachShader(program, shader);
+		}
+
+		gl::LinkProgram(program);
+
+		let mut success = 0;
+		gl::GetProgramiv(
+			program,
+			gl::LINK_STATUS,
+			&mut success,
+		);
+
+		if success == 0 {
+			Self::complain_about_shader();
+		}
+
+		if let Some(shader) = v_shader {
+			gl::DeleteShader(shader);
+		}
+
+		if let Some(shader) = g_shader {
+			gl::DeleteShader(shader);
+		}
+
+		if let Some(shader) = f_shader {
+			gl::DeleteShader(shader);
+		}
+
+		program
+	}}
+
+	fn build(&self) -> Shader {
+		let mut v_shader = None;
+		let mut g_shader = None;
+		let mut f_shader = None;
+
+		if let Some(v_src) = &self.vertex {
+			v_shader = Some(Self::compile_shader(&v_src, gl::VERTEX_SHADER));
+		}
+
+		if let Some(g_src) = &self.geom {
+			g_shader = Some(Self::compile_shader(&g_src, gl::GEOMETRY_SHADER));
+		}
+
+		if let Some(f_src) = &self.frag {
+			f_shader = Some(Self::compile_shader(&f_src, gl::FRAGMENT_SHADER));
+		}
+
+		Shader {
+			program: Self::create_program(v_shader, g_shader, f_shader),
+		}
+	}
+}
+
 pub struct Shader {
-    program: u32,
+	program: u32,
 }
 
 impl Shader {
-    pub fn new(vertex_path: &str, fragment_path: &str) -> Self {
-        let vertex_source = fs::read_to_string(vertex_path)
-            .unwrap_or_else(|e| {
-                panic!(
-                    "Failed to read vertex shader '{}': {}",
-                    vertex_path, e
-                )
-            });
+	pub fn bind(&self) {
+		unsafe {
+			gl::UseProgram(self.program);
+		}
+	}
 
-        let fragment_source = fs::read_to_string(fragment_path)
-            .unwrap_or_else(|e| {
-                panic!(
-                    "Failed to read fragment shader '{}': {}",
-                    fragment_path, e
-                )
-            });
+	fn get_uniform_location(&self, name: &str) -> i32 {
+		let c_name = CString::new(name)
+			.expect("Uniform name contained a null byte");
 
-        let vertex = Self::compile_shader(
-            &vertex_source,
-            gl::VERTEX_SHADER,
-        );
+		unsafe {
+			gl::GetUniformLocation(
+				self.program,
+				c_name.as_ptr(),
+			)
+		}
+	}
 
-        let fragment = Self::compile_shader(
-            &fragment_source,
-            gl::FRAGMENT_SHADER,
-        );
+	pub fn set_float(&self, name: &str, value: f32) {
+		let location = self.get_uniform_location(name);
 
-        let program;
+		unsafe {
+			gl::Uniform1f(location, value);
+		}
+	}
 
-        unsafe {
-            program = gl::CreateProgram();
+	pub fn set_int(&self, name: &str, value: i32) {
+		let location = self.get_uniform_location(name);
 
-            gl::AttachShader(program, vertex);
-            gl::AttachShader(program, fragment);
+		unsafe {
+			gl::Uniform1i(location, value);
+		}
+	}
 
-            gl::LinkProgram(program);
+	pub fn set_vec2(&self, name: &str, value: [f32; 2]) {
+		let location = self.get_uniform_location(name);
 
-            let mut success = 0;
-            gl::GetProgramiv(
-                program,
-                gl::LINK_STATUS,
-                &mut success,
-            );
+		unsafe {
+			gl::Uniform2f(
+				location,
+				value[0],
+				value[1],
+			);
+		}
+	}
 
-            if success == 0 {
-                let mut len = 0;
+	pub fn set_vec3(&self, name: &str, value: [f32; 3]) {
+		let location = self.get_uniform_location(name);
 
-                gl::GetProgramiv(
-                    program,
-                    gl::INFO_LOG_LENGTH,
-                    &mut len,
-                );
+		unsafe {
+			gl::Uniform3f(
+				location,
+				value[0],
+				value[1],
+				value[2],
+			);
+		}
+	}
 
-                let mut buffer = vec![0u8; len as usize];
+	pub fn set_vec4(&self, name: &str, value: [f32; 4]) {
+		let location = self.get_uniform_location(name);
 
-                gl::GetProgramInfoLog(
-                    program,
-                    len,
-                    ptr::null_mut(),
-                    buffer.as_mut_ptr() as *mut i8,
-                );
-
-                let error = String::from_utf8_lossy(&buffer);
-
-                panic!("Shader program linking failed:\n{}", error);
-            }
-
-            gl::DeleteShader(vertex);
-            gl::DeleteShader(fragment);
-        }
-
-        Self { program }
-    }
-
-    fn compile_shader(
-        source: &str,
-        shader_type: u32,
-    ) -> u32 {
-        let c_source = CString::new(source)
-            .expect("Shader source contained a null byte");
-
-        let shader;
-
-        unsafe {
-            shader = gl::CreateShader(shader_type);
-
-            gl::ShaderSource(
-                shader,
-                1,
-                &c_source.as_ptr(),
-                ptr::null(),
-            );
-
-            gl::CompileShader(shader);
-
-            let mut success = 0;
-
-            gl::GetShaderiv(
-                shader,
-                gl::COMPILE_STATUS,
-                &mut success,
-            );
-
-            if success == 0 {
-                let mut len = 0;
-
-                gl::GetShaderiv(
-                    shader,
-                    gl::INFO_LOG_LENGTH,
-                    &mut len,
-                );
-
-                let mut buffer = vec![0u8; len as usize];
-
-                gl::GetShaderInfoLog(
-                    shader,
-                    len,
-                    ptr::null_mut(),
-                    buffer.as_mut_ptr() as *mut i8,
-                );
-
-                let error = String::from_utf8_lossy(&buffer);
-
-                panic!("Shader compilation failed:\n{}", error);
-            }
-        }
-
-        shader
-    }
-
-    pub fn bind(&self) {
-        unsafe {
-            gl::UseProgram(self.program);
-        }
-    }
-
-    fn get_uniform_location(&self, name: &str) -> i32 {
-        let c_name = CString::new(name)
-            .expect("Uniform name contained a null byte");
-
-        unsafe {
-            gl::GetUniformLocation(
-                self.program,
-                c_name.as_ptr(),
-            )
-        }
-    }
-
-    pub fn set_float(&self, name: &str, value: f32) {
-        let location = self.get_uniform_location(name);
-
-        unsafe {
-            gl::Uniform1f(location, value);
-        }
-    }
-
-    pub fn set_int(&self, name: &str, value: i32) {
-        let location = self.get_uniform_location(name);
-
-        unsafe {
-            gl::Uniform1i(location, value);
-        }
-    }
-
-    pub fn set_vec2(&self, name: &str, value: [f32; 2]) {
-        let location = self.get_uniform_location(name);
-
-        unsafe {
-            gl::Uniform2f(
-                location,
-                value[0],
-                value[1],
-            );
-        }
-    }
-
-    pub fn set_vec3(&self, name: &str, value: [f32; 3]) {
-        let location = self.get_uniform_location(name);
-
-        unsafe {
-            gl::Uniform3f(
-                location,
-                value[0],
-                value[1],
-                value[2],
-            );
-        }
-    }
-
-    pub fn set_vec4(&self, name: &str, value: [f32; 4]) {
-        let location = self.get_uniform_location(name);
-
-        unsafe {
-            gl::Uniform4f(
-                location,
-                value[0],
-                value[1],
-                value[2],
-                value[3],
-            );
-        }
-    }
+		unsafe {
+			gl::Uniform4f(
+				location,
+				value[0],
+				value[1],
+				value[2],
+				value[3],
+			);
+		}
+	}
 }
 
 impl Drop for Shader {
-    fn drop(&mut self) {
-        unsafe {
-            gl::DeleteProgram(self.program);
-        }
-    }
+	fn drop(&mut self) {
+		unsafe {
+			gl::DeleteProgram(self.program);
+		}
+	}
 }
